19161bcd514016aa3a1abf04dbdede7a59429da1
Leonardo Kiszewski <kiszewski1999@gmail.com>
2024-11-02 18:57:41 -0300

    Implement Breadth Search Tree

--- CodewarChallenges.playground/Pages/BreadthSearchBTree.xcplaygroundpage/Contents.swift
+++ CodewarChallenges.playground/Pages/BreadthSearchBTree.xcplaygroundpage/Contents.swift
@@ -1,7 +1,94 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+class TreeNode<T> {
+    let value: T
+    var left: TreeNode<T>?
+    var right: TreeNode<T>?
+    
+    init(_ value: T, _ left: TreeNode<T>? = nil, _ right: TreeNode<T>? = nil) {
+        self.value = value
+        self.left = left
+        self.right = right
+    }
+}
 
-//: [Next](@next)
+func breadthSearch<T>(_ node: TreeNode<T>) {
+    let queue = Queue<TreeNode<T>>()
+    queue.enqueue(node)
+    
+    walk(node, queue)
+}
+
+func walk<T>(_ node: TreeNode<T>, _ queue: Queue<TreeNode<T>>) {
+    if node.left != nil { queue.enqueue(node.left!) }
+    if node.right != nil { queue.enqueue(node.right!) }
+    
+    print(node.value)
+    
+    queue.dequeue()
+    
+    if queue.peek() != nil { walk(queue.peek()!, queue) }
+}
+
+var root = TreeNode<Int>(1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(5, TreeNode(6), TreeNode(7)))
+
+breadthSearch(root)
+
+
+
+
+
+
+
+
+//Can't import, so have to copy & past
+
+class Node<T> {
+    let value: T
+    var next: Node<T>?
+    
+    init(value: T, next: Node<T>? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+class Queue<T> {
+    var lenght: Int = 0
+    var head: Node<T>?
+    var tail: Node<T>?
+    
+    func enqueue(_ value: T) {
+        lenght += 1
+        
+        let newNode = Node(value: value)
+        
+        if (head == nil) {
+            head = newNode
+            tail = newNode
+            
+            return
+        }
+        
+        tail?.next = newNode
+        tail = newNode
+        
+    }
+    
+    func dequeue() -> T? {
+        if (lenght > 0) {
+            lenght -= 1
+            
+            let oldHead = head
+            head = head?.next
+            
+            return oldHead?.value
+        }
+        
+        return nil
+    }
+    
+    func peek() -> T? {
+        head?.value
+    }
+}
--- CodewarChallenges.playground/Pages/DSStack.xcplaygroundpage/Contents.swift
+++ CodewarChallenges.playground/Pages/DSStack.xcplaygroundpage/Contents.swift
@@ -1,7 +1,73 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+class Node<T> {
+    let value: T
+    var previous: Node<T>?
+    
+    init(value: T, previous: Node<T>? = nil) {
+        self.value = value
+        self.previous = previous
+    }
+}
 
-//: [Next](@next)
+class Stack<T> {
+    private var head: Node<T>?
+    var length = 0
+    
+    func peek() -> T? {
+        head?.value
+    }
+    
+    func push(_ value: T) {
+        let node = Node(value: value)
+        
+        if length == 0 {
+            head = node
+        } else {
+            node.previous = head
+            head = node
+        }
+        
+        length += 1
+    }
+    
+    func pop() -> T? {
+        if length > 0 {
+            let oldHead = head
+            
+            if length == 1 {
+                head = nil
+            } else {
+                head = head?.previous
+            }
+            
+            length -= 1
+            
+            return oldHead?.value
+        } else {
+            return nil
+        }
+    }
+}
+
+let stack = Stack<Int>()
+
+stack.push(5)
+stack.push(7)
+stack.push(9)
+
+print(stack.pop() == 9)
+print(stack.length == 2)
+
+stack.push(11)
+
+print(stack.pop() == 11)
+print(stack.pop() == 7)
+print(stack.peek() == 5)
+print(stack.pop() == 5)
+print(stack.pop() == nil)
+
+// just wanted to make sure that I could not blow up myself when I remove everything
+stack.push(69)
+print(stack.peek() == 69)
+print(stack.length == 1)
